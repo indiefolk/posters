c6cc3025d7b1d69884e9dce7d1772e6fc01ecbad
John Lund <john.lund@piratalondon.com>
2013-05-15 15:38:42 +0100

    small animation on hover and added readme

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
 
+	var $main = $('.main');
+
+	$main.on('mouseover', function () {
+		$main.toggleClass('hover');
+
+	}).on('mouseout', function () {
+		$main.toggleClass('hover');
+	});
+
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Posters
+Creating graphic design ideas typically found in print, using CSS, JS & Illustrator
+
+Also a chance to practise my sweet Wacom skillz.
